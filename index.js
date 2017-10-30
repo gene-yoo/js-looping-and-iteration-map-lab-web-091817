07efcf42cdab1a7c45b2833920e7461d29101437
gene-yoo <gene.y.yoo@gmail.com>
2017-10-30 13:40:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution in this file.
+function lowerCaseDrivers(drivers) {
+  return drivers.map(function (driver) { return driver.toLowerCase(); });
+}
+
+function nameToAttributes (drivers) {
+  const splitNames = function (fullName) {
+    nameObj = {};
+
+    nameObj.firstName = fullName.split(" ")[0];
+    nameObj.lastName = fullName.split(" ")[1];
+    return nameObj;
+  }
+
+  return drivers.map(function (name) {return splitNames(name)});
+}
+
+function attributesToPhrase (drivers) {
+  return drivers.map(function (person) { return `${person.name} is from ${person.hometown}`; });
+}
